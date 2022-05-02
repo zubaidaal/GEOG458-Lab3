42524bcd4da573b9038fb319a28a413809851ee7
Zubaida Al Zubaidi <zubaidaal@Zs-MacBook.local>
2022-05-01 23:59:21 -0700

    draft

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# us-covid-2020
+# US COVID Data in 2020
+
+## Context
+
+This project build two thematic map that illustrate US COVID rates and case count in 2020, respectively. The data was collected regarding cases was collected in 2020 by the [New York Times](https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv). The cases correspond to population utilizing the population data gathered by the United States Census in 2018 as part of their 5 year estimates. Cases illustrated on the maps are on county level and are calculated as cases per thousand residents.
+
+
+## Map 1: US COVID Rates in 2020
+
+A choropleth map that displays the rate of covid cases at a county level in the United States.
+
+*photo*
+*link*
+
+## Map 2: US COVID Cases in 2020
+
+A proportional map that displays the case count of COVID cases at a county level in the United States.
+
+*photo*
+*link*
---
+++ js/main_1.js
@@ -0,0 +1,98 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+
+const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/light-v10', // style URL
+        zoom: 3, // starting zoom
+        center: [-100, 40] // starting center
+    }
+);
+
+async function geojsonFetch() { 
+    // other operations
+    let response = await fetch('../assets/us-covid-2020-rates.json');
+    let stateData = await response.json();
+    map.on('load', function loadingData() {
+        // add layer
+        // add legend
+        map.addSource('us-covid-2020-rates', {
+            type: 'geojson',
+            data: us-covid-2020-rates
+        });
+        
+        map.addLayer({
+            'id': 'us-covid-rates-2020-layer',
+            'type': 'fill',
+            'source': 'us-covid-2020-rates',
+            'paint': {
+                'fill-color': [
+                    'step',
+                    ['get', 'density'],
+                    '#FFEDA0',   // stop_output_0
+                    10,          // stop_input_0
+                    '#FED976',   // stop_output_1
+                    20,          // stop_input_1
+                    '#FEB24C',   // stop_output_2
+                    50,          // stop_input_2
+                    '#FD8D3C',   // stop_output_3
+                    100,         // stop_input_3
+                    '#FC4E2A',   // stop_output_4
+                    200,         // stop_input_4
+                    '#E31A1C',   // stop_output_5
+                    500,         // stop_input_5
+                    '#BD0026',   // stop_output_6
+                    1000,        // stop_input_6
+                    "#800026"    // stop_output_7
+                ],
+                'fill-outline-color': '#BBBBBB',
+                'fill-opacity': 0.7,
+            }
+        });
+    });
+ };
+
+geojsonFetch();
+
+const layers = [
+    '0-9',
+    '10-19',
+    '20-49',
+    '50-100',
+];
+const colors = [
+    '#FFEDA070',
+    '#FED97670',
+    '#FEB24C70',
+    '#FD8D3C70',
+];
+
+const legend = document.getElementById('legend');
+legend.innerHTML = "<b>Percentage<br>(people/sq.mi.)</b><br><br>";
+
+layers.forEach((layer, i) => {
+    const color = colors[i];
+    const item = document.createElement('div');
+    const key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
+
+    const value = document.createElement('span');
+    value.innerHTML = `${layer}`;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+});
+
+map.on('mousemove', ({point}) => {
+    const state = map.queryRenderedFeatures(point, {
+        layers: ['stateData-layer']
+    });
+    document.getElementById('text-description').innerHTML = state.length ?
+        `<h3>${state[0].properties.name}</h3><p><strong><em>${state[0].properties.density}</strong> people per square mile</em></p>` :
+        `<p>Hover over a state!</p>`;
+});
+
+const source =
+'<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">The New York Times</a></p>';
+
+legend.innerHTML = labels.join('') + source;
